d85bfa58c8d7973ef4dbfb8a56fa9885b5aaed92
Tom Flitterman <tfl@pers-ee.com>
2023-09-07 19:31:56 +0200

    Various improvements

--- src/components/layout/TheMovie.vue
+++ src/components/layout/TheMovie.vue
@@ -11,11 +11,10 @@
     </div>
 
     <div
-      class="h-[50vh] pb-16 pt-5 lg:w-1/2 lg:h-screen text-white duration-200 ease-out"
-      :class="{ 'grow bg-main-dark': isFullPage, 'custom-gradient-background lg:bg-main-dark': !isFullPage }"
+      class="h-[50vh] pb-16 pt-16 lg:w-1/2 lg:h-screen text-gray-200 duration-200 ease-out"
+      :class="{ 'grow bg-main-dark': isFullPage, 'custom-movie-bg lg:bg-main-dark': !isFullPage }"
     >
       <div class="flex flex-col h-full">
-
         <div v-if="isFullPage" class="self-center lg:hidden">
           <button @click="toggleMoviePage">
             <Chevron
@@ -32,20 +31,20 @@
         >
           <!-- left (desktop) -->
           <div
-            class="lg:w-1/2 flex flex-col"
+            class="lg:w-1/2 flex flex-col lg:sticky top-0"
             :class="{ 'lg:w-full': !isFullPage }"
           >
-            <div class="grow">
+            <div class="grow lg:w-3/4 lg:mx-auto overflow-y-scroll">
               <!-- [FREE] Movie name -->
-              <div class="flex items-baseline px-2 mb-4">
-                <p
-                  v-if="movie.freeToWatch"
-                  class="bg-gray-100 px-2 text-black text-xl rounded mr-2"
-                >
-                  Free
-                </p>
-                <h2 class="text-4xl">{{ movie.name }}</h2>
-              </div>
+              <h2 class="px-6 lg:px-0 mb-4 text-4xl font-bold text-main-red">
+                  <span
+                    v-if="movie.freeToWatch"
+                    class="mr-2 px-3 pb-0.5 bg-gray-100 text-main-dark font-bold relative bottom-1 text-xl rounded"
+                  >
+                    Free
+                </span>
+                {{ movie.name }}
+              </h2>
 
               <!-- Full movie (free movie) - Mobile -->
               <div
@@ -63,8 +62,8 @@
               </div>
 
               <!-- Genres -->
-              <p class="px-2 mb-3 font-bold text-xl">
-                {{ movie.year }} •
+              <p class="px-6 lg:px-0 mb-3 font-bold text-xl">
+                {{ movie.year }} |
                 <span
                   v-for="(genre, i) in movie.genres"
                   :key="genre"
@@ -73,10 +72,10 @@
                 </span>
               </p>
 
-              <p class="px-4 mb-3 text-justify text-lg text-gray-200 italic">{{ movie.description }}</p>
+              <p class="px-8 lg:px-0 mb-3 text-justify text-lg text-gray-300 italic">{{ movie.description }}</p>
 
               <!-- Actors -->
-              <p class="px-2 font-bold text-lg">
+              <p class="px-6 mb-6 lg:px-0 font-bold text-lg">
                 <span
                   v-for="(actor, i) in movie.actors"
                   :key="actor"
@@ -87,7 +86,7 @@
             </div>
 
             <!-- Open/Close (desktop) -->
-            <div class="hidden lg:block grow-0 text-center text-lg font-bold">
+            <div class="hidden lg:block text-center text-lg font-bold">
               <button @click="toggleMoviePage">
                 <span v-if="isFullPage">
                   <span class="border-b">See less</span> <Chevron direction="right" />
@@ -101,19 +100,21 @@
           </div>
 
           <!-- Right (desktop) -->
-          <div :class="{ '!lg:w-0 lg:overflow-y-hidden': !isFullPage, 'lg:w-1/2': isFullPage }">
+          <div :class="{ 'lg:w-0 lg:overflow-y-hidden': !isFullPage, 'lg:w-1/2': isFullPage }">
             <!-- Full movie (free movie) - Mobile -->
-            <div
-              class="relative h-0 overflow-hidden max-w-full w-full mb-2 hidden lg:block"
-              style="padding-bottom: 56.25%"
-            >
-              <iframe
-                class="absolute top-0 left-0 w-full h-full"
-                :src="`https://www.youtube-nocookie.com/embed/${videoId(movie.watch ? movie.watch.url : movie.trailer.url)}`"
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowfullscreen
-              ></iframe>
+            <div class="px-6">
+              <div
+                class="relative h-0 overflow-hidden max-w-full w-full mb-2 hidden lg:block"
+                style="padding-bottom: 56.25%"
+              >
+                <iframe
+                  class="absolute top-0 left-0 w-full h-full"
+                  :src="`https://www.youtube-nocookie.com/embed/${videoId(movie.watch ? movie.watch.url : movie.trailer.url)}`"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowfullscreen
+                ></iframe>
+              </div>
             </div>
 
             <!-- Reviews -->
@@ -148,15 +149,15 @@ const props = defineProps(['movie']);
 const movie = props.movie;
 
 const router = useRouter();
-const movies = useMoviesStore();
+const store = useMoviesStore();
 
 const movieDescription = ref(null);
 const isFullPage = ref(false);
 
 onBeforeMount(() => {
-  if (!movies.decade) {
-    movies.decade = Math.floor(parseInt(movie.year) / 10) * 10;
-    document.getElementById(movies.decade).scrollIntoView();
+  if (!store.decade) {
+    store.decade = Math.floor(parseInt(movie.year) / 10) * 10;
+    document.getElementById(store.decade).scrollIntoView();
   }
 
   if (window.location.pathname.slice(0, 2) === '/m') {
@@ -170,9 +171,9 @@ async function toggleMoviePage() {
     movieDescription.value.scrollTo({ top: 0, smooth: true });
     window.history.pushState({}, '', `/`);
 
-    if (!movies.movies.length) {
-      movies.movies.unshift(movie);
-      await movies.fetchMovies();
+    if (!store.movies.length) {
+      store.movies.unshift(movie);
+      await store.fetchMovies();
     }
 
     setTimeout(() => router.push('/'), 210);
